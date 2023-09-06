553ca526b4a7fee20b5b16da45457713017c8bb6
Leza Younan <Lezayounan@gmail.com>
2023-09-05 23:07:15 -0500

    first commit

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+
+
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+load_dotenv(os.path.join(basedir, '.env'))
+
+
+class Config():
+
+    """
+    Set Config variables for our Flask app.
+    Using Environment variables where available otherwise
+    Create config variables
+    """
+
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    FLASK_DEBUG = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or "osca suki ojitos bellamy ace"
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
